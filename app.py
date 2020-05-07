f11314109063c4379dac9fe82fbe07e4ed3fdaa7
kcpbantu <info@kcpbantu.org>
2020-05-08 06:32:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ def checkout():
   intent = \Stripe\Stripe::setApiKey('pk_live_y0w739w1dR6yZv3P8dfhFBCh00lEbc72UX');
 
 $payment_intent = \Stripe\PaymentIntent::create([
-  'payment_method_types' => ['fpx'],
-  'amount' => 1000,
+  'payment_method_types' => ['fpx']['card'],
+  'amount' => 10000,
   'currency' => 'myr',
 ]);
   return render_template('checkout.html', client_secret=intent.client_secret)
